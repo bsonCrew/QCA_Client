cd86b28b695956d5741e52d01392d77a67eccf6c
pinkishincoloragain <smb1103@gmail.com>
2022-08-27 01:26:35 +0900

    #15 feature: added cases for double links in description

--- qca/src/Components/atom/CardDialog.js
+++ qca/src/Components/atom/CardDialog.js
@@ -1,27 +1,69 @@
 import * as React from "react";
 import Dialog from "@mui/material/Dialog";
+import config from "../../config.json";
 
 const RE_LEARNMORE = /\[[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\s]+\]|\[[a-zA-Z\s]+\]/g;
 const RE_URL =
 	/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g;
 function linkify(str) {
 	let results = [];
-	const matchesLearnmore = [...str.matchAll(RE_LEARNMORE)];
-	const matchesURL = [...str.matchAll(RE_URL)];
-	// console.log(matchesLearnmore, matchesURL);
+	str.split(". ").forEach(spl => {
+		const matchesLearnmore = [...spl.matchAll(RE_LEARNMORE)][0];
+		const matchesURL = [...spl.matchAll(RE_URL)][0];
 
-	if (matchesLearnmore !== null) {
-		matchesLearnmore.forEach((match, idx) => {
-			console.log(matchesURL[0]);
-			// if (matchesURL[idx][0] !== null && matchesURL[idx][0] !== 0) {
-			results.push(
-				<a href={matchesURL[0][0]} target="_blank" rel="noopener noreferrer">
-					{match[0]}
-				</a>
+		console.log(matchesLearnmore, matchesURL);
+
+		if (matchesLearnmore !== undefined && matchesURL !== undefined) {
+			const beforeText = matchesLearnmore.input.slice(0, spl.indexOf("["));
+			const afterText = matchesLearnmore.input.slice(
+				spl.indexOf(")") + 1,
+				spl.length
 			);
-			// }
-		});
-	}
+
+			const URL = matchesURL[0].slice(
+				matchesURL[0].indexOf("(") + 1,
+				matchesURL[0].indexOf(")")
+			);
+			results.push(
+				<React.Fragment key={spl.length}>
+					{beforeText}
+					<a
+						style={{
+							color: config.colors.purple,
+						}}
+						key={URL.length}
+						href={URL}
+						target="_blank"
+						rel="noreferrer"
+					>
+						{matchesLearnmore[0]}
+					</a>
+					{afterText}
+					{afterText.length > 0 ? ". " : ""}
+				</React.Fragment>
+			);
+		} else results.push(<span key={spl.length}>{spl}.&nbsp;</span>);
+
+		// if (matchesLearnmore !== null) {
+		// 	matchesLearnmore.forEach((match, idx) => {
+		// 		if (match !== null && match !== undefined) {
+		// 			match[0] = match[0].replace(")");
+		// 			console.log(match, idx);
+		// 		}
+		// 		results.push(
+		// 			<a
+		// 				key={match[0]}
+		// 				href={matchesURL[0][0]}
+		// 				target="_blank"
+		// 				rel="noopener noreferrer"
+		// 			>
+		// 				{match[0]}
+		// 			</a>
+		// 		);
+		// 		// }
+		// 	});
+		// }
+	});
 
 	// match2 = match.input;
 	// if (match !== null) {
@@ -33,22 +75,7 @@ function linkify(str) {
 	// console.log("linkText: ", match[0]);
 	// console.log("afterText: ", afterText);
 
-	// results.unshift(
-	// 	<span key={str.length}>
-	// 		{beforeText}
-	// 		<a
-	// 			style={{
-	// 				color: "red",
-	// 			}}
-	// 			key={match[0].length}
-	// 			href={linkHref}
-	// 		>
-	// 			{match[0]}
-	// 		</a>
-	// 		{afterText}
-	// 	</span>
-	// );
-	// } else results.push(<span key={str.length}>{str}</span>);
+	//
 
 	return results;
 }
@@ -71,7 +98,7 @@ export default function CardDialog(props) {
 			// onClose={handleClose}
 		>
 			<div className="w-fit h-96 p-12">
-				<span className="text-4xl font-bold">{props.title}</span>
+				<span className="text-3xl font-bold">{props.title}</span>
 				<br />
 				<br />
 				<span className="text-xl pt-4 leading-8">{props.subheader}</span>
