63de94f773949882d3646b87fe5c25440ece67f5
pinkishincoloragain <smb1103@gmail.com>
2022-08-30 15:07:41 +0900

    #17 feature: added criteria id and mapping for calculation

--- qca/src/Components/utils/calculate.js
+++ qca/src/Components/utils/calculate.js
@@ -10,7 +10,6 @@ const createCriteriaMap = mapId => {
 			return [key, rowItem];
 		})
 	);
-
 	return criteriaMap;
 	// return [accessibility, compatibility, connectivity, openness];
 };
@@ -18,20 +17,41 @@ const createCriteriaMap = mapId => {
 const calculate = lighthouseResults => {
 	/** 0: accessibility, 1: compatibility, 2: connectivity, 3: openness */
 
-	const [accessibility, compatibility, connectivity, openness] = [
+	const [
+		accessibility,
+		compatibility,
+		connectivity,
+		openness,
+		enhancement,
+		warning,
+	] = [
 		"accessibility",
 		"compatibility",
 		"connectivity",
 		"openness",
+		"enhancement",
+		"warning",
 	].map(_ => createCriteriaMap(_));
-	console.log(accessibility, compatibility, connectivity, openness);
+	const criterias = {
+		accessibility,
+		compatibility,
+		connectivity,
+		openness,
+		enhancement,
+		warning,
+	};
 
 	lighthouseResults.forEach(lr => {
 		if (audits.audits[lr.id] !== undefined) {
+			const audit = audits.audits[lr.id];
+			const criteria = criterias[audit.class].get(audit.subClass)[
+				audit.criteriaId
+			];
+			criteria.scores.push(lr.score);
+			console.log(lr.score);
 		}
 	});
-
-	// console.log(accessibility, compatibility, connectivity, openness);
+	console.log(criterias);
 };
 
 export default calculate;
