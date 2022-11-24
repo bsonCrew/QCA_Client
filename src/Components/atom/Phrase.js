60cd37275473a5fc33c63b3c0020286d9660f17b
pinkishincoloragain <smb1103@gmail.com>
2022-11-25 01:06:02 +0900

    #48 feat: removed tailwind from Phrase

--- src/Components/atom/Phrase.js
+++ src/Components/atom/Phrase.js
@@ -1,11 +1,18 @@
-import { HBlue, H3Gray } from '../../Themes/CustomStyled';
+import { HBlue, H3Gray } from "../../Themes/CustomStyled";
+import styled from "@emotion/styled";
+
+const PhraseWrapper = styled.div({
+  widht: "100%",
+  display: "flex",
+  flexDirection: "column",
+});
 
 const Phrase = ({ title, subtitle }) => {
   return (
-    <div className='w-full flex flex-col'>
+    <PhraseWrapper>
       <HBlue>{title}</HBlue>
       <H3Gray>{subtitle}</H3Gray>
-    </div>
+    </PhraseWrapper>
   );
 };
 
